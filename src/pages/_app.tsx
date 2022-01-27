b0df3e60ab4b719f7f496b1524e6d9244cbf8352
Yerma <yerma.araya@gmail.com>
2022-01-26 21:56:22 -0300

    Update favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import '../styles/globals.scss'
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
     <Head key="main">
-      <link rel="icon" type="image/png" href="img/icono.ico" />
+      <link rel="icon" type="image/png" href="favicon.ico" />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" />
       <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Euphoria+Script|Roboto+Mono:300,400,500,700" rel="stylesheet" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" />
